b6debd8d70b41d6584eeeb396fdae2b4b927f973
P1nt056 <ropinto06@gmail.com>
2024-11-24 21:51:17 +0000

    Update cliente.c

--- cliente.c
+++ cliente.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include "cliente.h"
 
-// Lista dinâmica de clientes
-Cliente *clientes = NULL;
+// Variáveis globais específicas deste arquivo
+int cliente_ids[MAX_CLIENTES];
+char cliente_nomes[MAX_CLIENTES][TAM_NOME];
+char cliente_contatos[MAX_CLIENTES][TAM_CONTATO];
+char cliente_nifs[MAX_CLIENTES][TAM_NIF];
+char cliente_datas_registro[MAX_CLIENTES][TAM_DATA];
 int total_clientes_cadastrados = 0;
 
-// Função auxiliar para obter a data atual no formato "dd/mm/aaaa"
-void obter_data_atual(char *data) {
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    sprintf(data, "%02d/%02d/%04d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
-}
-
 void criar_cliente() {
+    if (total_clientes_cadastrados >= MAX_CLIENTES) {
+        printf("Limite máximo de clientes alcançado.\n");
+        return;
+    }
+
+    cliente_ids[total_clientes_cadastrados] = total_clientes_cadastrados + 1;
+
     printf("\n--- Criar Cliente ---\n");
-
-    // Realocação dinâmica para um novo cliente
-    clientes = realloc(clientes, (total_clientes_cadastrados + 1) * sizeof(Cliente));
-    Cliente *novo_cliente = &clientes[total_clientes_cadastrados];
-    novo_cliente->id = total_clientes_cadastrados + 1;
-
     printf("Nome do Cliente: ");
-    scanf(" %[^\n]s", novo_cliente->nome);
+    scanf(" %[^\n]s", cliente_nomes[total_clientes_cadastrados]);
 
     printf("Contato (E-mail ou Telefone): ");
-    scanf(" %[^\n]s", novo_cliente->contato);
+    scanf(" %[^\n]s", cliente_contatos[total_clientes_cadastrados]);
 
     printf("NIF (Número de Identificação Fiscal): ");
-    scanf(" %[^\n]s", novo_cliente->nif);
+    scanf(" %[^\n]s", cliente_nifs[total_clientes_cadastrados]);
 
-    // Geração automática da data de registro
-    obter_data_atual(novo_cliente->data_registro);
+    printf("Data de Registro (dd/mm/aaaa): ");
+    scanf(" %[^\n]s", cliente_datas_registro[total_clientes_cadastrados]);
 
     total_clientes_cadastrados++;
     printf("Cliente cadastrado com sucesso!\n");
@@ -48,9 +43,8 @@ void listar_clientes() {
     }
 
     for (int i = 0; i < total_clientes_cadastrados; i++) {
-        Cliente *c = &clientes[i];
-        printf("ID: %d | Nome: %s | Contato: %s | NIF: %s | Data de Registro: %s\n", 
-               c->id, c->nome, c->contato, c->nif, c->data_registro);
+        printf("ID: %d | Nome: %s | Contato: %s | NIF: %s | Data de Registro: %s\n",
+               cliente_ids[i], cliente_nomes[i], cliente_contatos[i], cliente_nifs[i], cliente_datas_registro[i]);
     }
 }
 
@@ -65,15 +59,17 @@ void atualizar_cliente() {
         return;
     }
 
-    Cliente *c = &clientes[id - 1];
-    printf("Novo Nome (atual: %s): ", c->nome);
-    scanf(" %[^\n]s", c->nome);
+    printf("Novo Nome (atual: %s): ", cliente_nomes[id - 1]);
+    scanf(" %[^\n]s", cliente_nomes[id - 1]);
 
-    printf("Novo Contato (atual: %s): ", c->contato);
-    scanf(" %[^\n]s", c->contato);
+    printf("Novo Contato (atual: %s): ", cliente_contatos[id - 1]);
+    scanf(" %[^\n]s", cliente_contatos[id - 1]);
 
-    printf("Novo NIF (atual: %s): ", c->nif);
-    scanf(" %[^\n]s", c->nif);
+    printf("Novo NIF (atual: %s): ", cliente_nifs[id - 1]);
+    scanf(" %[^\n]s", cliente_nifs[id - 1]);
+
+    printf("Nova Data de Registro (atual: %s): ", cliente_datas_registro[id - 1]);
+    scanf(" %[^\n]s", cliente_datas_registro[id - 1]);
 
     printf("Cliente atualizado com sucesso!\n");
 }
@@ -89,13 +85,23 @@ void excluir_cliente() {
         return;
     }
 
-    for (int i = id; i < total_clientes_cadastrados; i++) {
-        clientes[i - 1] = clientes[i];
+    for (int i = id - 1; i < total_clientes_cadastrados - 1; i++) {
+        cliente_ids[i] = cliente_ids[i + 1];
+        for (int j = 0; j < TAM_NOME; j++) {
+            cliente_nomes[i][j] = cliente_nomes[i + 1][j];
+        }
+        for (int j = 0; j < TAM_CONTATO; j++) {
+            cliente_contatos[i][j] = cliente_contatos[i + 1][j];
+        }
+        for (int j = 0; j < TAM_NIF; j++) {
+            cliente_nifs[i][j] = cliente_nifs[i + 1][j];
+        }
+        for (int j = 0; j < TAM_DATA; j++) {
+            cliente_datas_registro[i][j] = cliente_datas_registro[i + 1][j];
+        }
     }
 
     total_clientes_cadastrados--;
-    clientes = realloc(clientes, total_clientes_cadastrados * sizeof(Cliente));
-
     printf("Cliente excluído com sucesso!\n");
 }
 
@@ -106,6 +112,4 @@ void total_clientes() {
 void clientes_mais_ativos() {
     printf("\n--- Clientes Mais Ativos ---\n");
     printf("Função em desenvolvimento.\n");
-    // A lógica será baseada no número de encomendas associadas a cada cliente
-    // quando o módulo Encomenda for implementado.
 }
