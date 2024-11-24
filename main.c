2ed5b9b154023d3e795d1c18448af83bcc09f803
P1nt056 <ropinto06@gmail.com>
2024-11-24 18:40:31 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "produto.h" // Inclui as definições e funções de produtos
 
-// Prototipação de funções principais
+// Prototipação dos submenus
 void menu_produto();
-void menu_cliente();
-void menu_encomenda();
-void menu_maquina();
 
 int main() {
     int opcao;
@@ -14,9 +11,9 @@ int main() {
     do {
         printf("\n--- Sistema de Gestão de Encomendas ---\n");
         printf("1. Gestão de Produtos\n");
-        printf("2. Gestão de Clientes\n");
-        printf("3. Gestão de Encomendas\n");
-        printf("4. Gestão de Máquinas\n");
+        printf("2. Gestão de Clientes (Em Desenvolvimento)\n");
+        printf("3. Gestão de Encomendas (Em Desenvolvimento)\n");
+        printf("4. Gestão de Máquinas (Em Desenvolvimento)\n");
         printf("0. Sair\n");
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
@@ -24,15 +21,6 @@ int main() {
         switch (opcao) {
             case 1:
                 menu_produto();
-                break;
-            case 2:
-                menu_cliente();
-                break;
-            case 3:
-                menu_encomenda();
-                break;
-            case 4:
-                menu_maquina();
                 break;
             case 0:
                 printf("Saindo do sistema...\n");
@@ -43,4 +31,52 @@ int main() {
     } while (opcao != 0);
 
     return 0;
+}
+
+// Submenu de Gestão de Produtos
+void menu_produto() {
+    int opcao;
+
+    do {
+        printf("\n--- Gestão de Produtos ---\n");
+        printf("1. Criar Produto\n");
+        printf("2. Listar Produtos\n");
+        printf("3. Atualizar Produto\n");
+        printf("4. Excluir Produto\n");
+        printf("5. Total de Produtos\n");
+        printf("6. Produtos por Categoria\n");
+        printf("7. Produtos Mais/Menos Procurados\n");
+        printf("0. Voltar ao Menu Principal\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                criar_produto();
+                break;
+            case 2:
+                listar_produtos();
+                break;
+            case 3:
+                atualizar_produto();
+                break;
+            case 4:
+                excluir_produto();
+                break;
+            case 5:
+                total_produtos();
+                break;
+            case 6:
+                produtos_por_categoria();
+                break;
+            case 7:
+                produtos_mais_menos_procurados();
+                break;
+            case 0:
+                printf("Voltando ao menu principal...\n");
+                break;
+            default:
+                printf("Opção inválida! Tente novamente.\n");
+        }
+    } while (opcao != 0);
 }
