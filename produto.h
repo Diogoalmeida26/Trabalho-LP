02a39b9873cddfdb58345276346ec775c3304088
P1nt056 <ropinto06@gmail.com>
2024-11-24 21:51:52 +0000

    Update produto.h

--- produto.h
+++ produto.h
@@ -1,27 +1,16 @@
 #ifndef PRODUTO_H
 #define PRODUTO_H
 
-// Definição das estruturas
-typedef struct {
-    int id_maquina;
-    int tempo_execucao;
-} Processo;
+// Máximo de produtos e tamanho máximo dos campos
+#define MAX_PRODUTOS 100
+#define TAM_NOME_PROD 50
+#define TAM_CATEGORIA 30
 
-typedef struct {
-    int id;
-    char nome[50];
-    char categoria[20];
-    Processo *processos; // Lista dinâmica de processos
-    int num_processos;
-} Produto;
-
-// Funções CRUD
+// Protótipos das funções relacionadas a produtos
 void criar_produto();
 void listar_produtos();
 void atualizar_produto();
 void excluir_produto();
-
-// Relatórios
 void total_produtos();
 void produtos_por_categoria();
 void produtos_mais_menos_procurados();
