2d96663c569217071fa73924b87b7012d62dacac
P1nt056 <ropinto06@gmail.com>
2024-11-24 18:59:58 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "produto.h"
-#include "cliente.h"
-#include "encomenda.h"
+#include "produto.h" // Inclui as definições e funções de produtos
 
 // Prototipação dos submenus
 void menu_produto();
-void menu_cliente();
-void menu_encomenda();
 
 int main() {
     int opcao;
@@ -15,8 +11,8 @@ int main() {
     do {
         printf("\n--- Sistema de Gestão de Encomendas ---\n");
         printf("1. Gestão de Produtos\n");
-        printf("2. Gestão de Clientes\n");
-        printf("3. Gestão de Encomendas\n");
+        printf("2. Gestão de Clientes (Em Desenvolvimento)\n");
+        printf("3. Gestão de Encomendas (Em Desenvolvimento)\n");
         printf("4. Gestão de Máquinas (Em Desenvolvimento)\n");
         printf("0. Sair\n");
         printf("Escolha uma opção: ");
@@ -25,12 +21,6 @@ int main() {
         switch (opcao) {
             case 1:
                 menu_produto();
-                break;
-            case 2:
-                menu_cliente();
-                break;
-            case 3:
-                menu_encomenda();
                 break;
             case 0:
                 printf("Saindo do sistema...\n");
@@ -43,6 +33,95 @@ int main() {
     return 0;
 }
 
+// Submenu de Gestão de Produtos
+void menu_produto() {
+    int opcao;
+
+    do {
+        printf("\n--- Gestão de Produtos ---\n");
+        printf("1. Criar Produto\n");
+        printf("2. Listar Produtos\n");
+        printf("3. Atualizar Produto\n");
+        printf("4. Excluir Produto\n");
+        printf("5. Total de Produtos\n");
+        printf("6. Produtos por Categoria\n");
+        printf("7. Produtos Mais/Menos Procurados\n");
+        printf("0. Voltar ao Menu Principal\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                criar_produto();
+                break;
+            case 2:
+                listar_produtos();
+                break;
+            case 3:
+                atualizar_produto();
+                break;
+            case 4:
+                excluir_produto();
+                break;
+            case 5:
+                total_produtos();
+                break;
+            case 6:
+                produtos_por_categoria();
+                break;
+            case 7:
+                produtos_mais_menos_procurados();
+                break;
+            case 0:
+                printf("Voltando ao menu principal...\n");
+                break;
+            default:
+                printf("Opção inválida! Tente novamente.\n");
+        }
+    } while (opcao != 0);
+}
+void menu_cliente() {
+    int opcao;
+
+    do {
+        printf("\n--- Gestão de Clientes ---\n");
+        printf("1. Criar Cliente\n");
+        printf("2. Listar Clientes\n");
+        printf("3. Atualizar Cliente\n");
+        printf("4. Excluir Cliente\n");
+        printf("5. Total de Clientes\n");
+        printf("6. Clientes Mais Ativos\n");
+        printf("0. Voltar ao Menu Principal\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                criar_cliente();
+                break;
+            case 2:
+                listar_clientes();
+                break;
+            case 3:
+                atualizar_cliente();
+                break;
+            case 4:
+                excluir_cliente();
+                break;
+            case 5:
+                total_clientes();
+                break;
+            case 6:
+                clientes_mais_ativos();
+                break;
+            case 0:
+                printf("Voltando ao menu principal...\n");
+                break;
+            default:
+                printf("Opção inválida! Tente novamente.\n");
+        }
+    } while (opcao != 0);
+}
 void menu_encomenda() {
     int opcao;
 
@@ -72,6 +151,48 @@ void menu_encomenda() {
                 break;
             case 5:
                 listar_encomendas_por_estado();
+                break;
+            case 0:
+                printf("Voltando ao menu principal...\n");
+                break;
+            default:
+                printf("Opção inválida! Tente novamente.\n");
+        }
+    } while (opcao != 0);
+}
+void menu_maquina() {
+    int opcao;
+
+    do {
+        printf("\n--- Gestão de Máquinas ---\n");
+        printf("1. Criar Máquina\n");
+        printf("2. Listar Máquinas\n");
+        printf("3. Atualizar Máquina\n");
+        printf("4. Excluir Máquina\n");
+        printf("5. Máquinas Mais Utilizadas\n");
+        printf("6. Tempo Médio por Máquina\n");
+        printf("0. Voltar ao Menu Principal\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                criar_maquina();
+                break;
+            case 2:
+                listar_maquinas();
+                break;
+            case 3:
+                atualizar_maquina();
+                break;
+            case 4:
+                excluir_maquina();
+                break;
+            case 5:
+                maquinas_mais_utilizadas();
+                break;
+            case 6:
+                tempo_medio_por_maquina();
                 break;
             case 0:
                 printf("Voltando ao menu principal...\n");
