c771a81d28129537c7668e99dbe4f6cb6da0bba0
P1nt056 <ropinto06@gmail.com>
2024-12-01 18:09:38 +0000

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,10 +1,12 @@
 #include "menu.h"
 #include "produto.h"
 #include "cliente.h"
+#include "encomendas.h"
 #include "input.h"
 
 #include <stdio.h>
 
+// Submenu de Produtos
 void menu_produtos() {
     int opcao, id, quantidade;
     char nome[TAM_NOME_PROD], categoria[TAM_CATEGORIA];
@@ -21,7 +23,7 @@ void menu_produtos() {
         printf("0. Voltar ao Menu Principal\n");
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
-        clearInputBuffer(); // Limpa o buffer de entrada
+        clearInputBuffer();
 
         switch (opcao) {
             case 1:
@@ -30,7 +32,7 @@ void menu_produtos() {
                 clearInputBuffer();
                 printf("Nome: ");
                 fgets(nome, TAM_NOME_PROD, stdin);
-                nome[strcspn(nome, "\n")] = '\0'; // Remove o newline
+                nome[strcspn(nome, "\n")] = '\0';
                 printf("Categoria: ");
                 fgets(categoria, TAM_CATEGORIA, stdin);
                 categoria[strcspn(categoria, "\n")] = '\0';
@@ -81,6 +83,7 @@ void menu_produtos() {
     } while (opcao != 0);
 }
 
+// Submenu de Clientes
 void menu_clientes() {
     int opcao, id;
     char nome[TAM_NOME], contato[TAM_CONTATO], nif[TAM_NIF], data[TAM_DATA];
@@ -151,6 +154,96 @@ void menu_clientes() {
                     printf("%s\n", lista[i]);
                 }
                 break;
+
+            case 0:
+                printf("Voltando ao menu principal...\n");
+                break;
+
+            default:
+                printf("Opção inválida!\n");
+        }
+    } while (opcao != 0);
+}
+
+// Submenu de Encomendas
+void menu_encomendas() {
+    int opcao, id, cliente_id, estado, quantidade;
+    char data[TAM_DATA];
+    int lista_ids[MAX_ENCOMENDAS];
+    char lista_datas[MAX_ENCOMENDAS][TAM_DATA];
+    float taxa;
+
+    do {
+        printf("\n--- Menu de Encomendas ---\n");
+        printf("1. Criar Encomenda\n");
+        printf("2. Listar Encomendas\n");
+        printf("3. Listar Encomendas por Estado\n");
+        printf("4. Atualizar Encomenda\n");
+        printf("5. Excluir Encomenda\n");
+        printf("6. Relatório de Encomendas por Estado\n");
+        printf("0. Voltar ao Menu Principal\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+        clearInputBuffer();
+
+        switch (opcao) {
+            case 1:
+                printf("ID do Cliente: ");
+                scanf("%d", &cliente_id);
+                clearInputBuffer();
+                printf("Data (dd/mm/aaaa): ");
+                fgets(data, TAM_DATA, stdin);
+                data[strcspn(data, "\n")] = '\0';
+                printf("Prioridade (0: Baixa, 1: Média, 2: Alta): ");
+                scanf("%d", &estado);
+                criar_encomenda(cliente_id, data, estado);
+                break;
+
+            case 2:
+                listar_encomendas(lista_datas, &quantidade);
+                printf("Encomendas:\n");
+                for (int i = 0; i < quantidade; i++) {
+                    printf("Data: %s\n", lista_datas[i]);
+                }
+                break;
+
+            case 3:
+                printf("Estado (0: Aberta, 1: Produção, 2: Finalizada): ");
+                scanf("%d", &estado);
+                encomendas_por_estado(estado, lista_ids, &quantidade);
+                printf("Encomendas no estado %d:\n", estado);
+                for (int i = 0; i < quantidade; i++) {
+                    printf("ID: %d\n", lista_ids[i]);
+                }
+                break;
+
+            case 4:
+                printf("ID da Encomenda: ");
+                scanf("%d", &id);
+                printf("Novo Estado (0: Aberta, 1: Produção, 2: Finalizada): ");
+                scanf("%d", &estado);
+                if (estado == 1) {
+                    printf("Taxa de Cumprimento: ");
+                    scanf("%f", &taxa);
+                } else {
+                    taxa = 0.0;
+                }
+                atualizar_encomenda(id, estado, taxa);
+                break;
+
+            case 5:
+                printf("ID da Encomenda a excluir: ");
+                scanf("%d", &id);
+                excluir_encomenda(id);
+                break;
+
+            case 6: {
+                int abertas, producao, finalizadas;
+                relatorio_encomendas_por_estado(&abertas, &producao, &finalizadas);
+                printf("Relatório de Encomendas:\n");
+                printf("Abertas: %d, Produção: %d, Finalizadas: %d\n", abertas, producao, finalizadas);
+                break;
+            }
 
             case 0:
                 printf("Voltando ao menu principal...\n");
