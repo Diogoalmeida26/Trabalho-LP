f6a15d47d8ba806b8ed8d881dfe42b5a508eeb21
P1nt056 <ropinto06@gmail.com>
2024-12-01 14:52:10 +0000

    Create input.h

---
+++ input.h
@@ -0,0 +1,16 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define VALOR_INVALIDO "O valor inserido é inválido"
+#define MAX_INT 2147483647
+#define SIZE_BUFFER 100
+#define MENU "+------------------------+"
+#define CODE 3500
+
+void cleanInputBuffer();
+
+#endif //INPUT_H
+
