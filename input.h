3e50d0d89d8785151c06a1f9cf6a1e3c9ad039ac
P1nt056 <ropinto06@gmail.com>
2024-12-05 17:33:59 +0000

    Update input.h

--- input.h
+++ input.h
@@ -5,25 +5,18 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
-void cleanInputBuffer();
+
+void clearInputBuffer();
 int getInt(int minValue, int maxValue, const char *msg);
 float getFloat(float minValue, float maxValue, const char *msg);
 char getChar(const char *msg);
 void readString(char *str, unsigned int size, const char *msg);
+void lerNumeroNaoZero(char *numero, const char *msg); // Adicionado protótipo da função
 
-typedef struct{
+typedef struct {
     int dia;
     int mes;
     int ano;
-}data;
+} data;
 
-typedef struct{
-    int id[11];
-    char nome[50];
-    int tele[10];
-    int nif[10];
-    data d;
-}cliente;
-
-#endif //INPUT_H
-
+#endif // INPUT_H
