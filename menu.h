6261ec25c608f7abc6f7eb4814f21af1ce804a77
Diogoalmeida26 <8240336@estg.ipp.pt>
2025-12-10 15:20:13 +0000

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,11 +1,1 @@
-#ifndef MENU_H
-#define MENU_H
 
-void menu_principal();
-void menu_clientes();
-void menu_espacos();
-void menu_reservas();
-void menu_equipamentos();
-void menu_relatorios();
-
-#endif
