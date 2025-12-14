69e3e00f0e753c0d0305caf6aa04bc070d12b007
P1nt056 <ropinto06@gmail.com>
2025-12-14 14:05:04 +0000

    Update validacao.h

--- validacao.h
+++ validacao.h
@@ -1,1 +1,21 @@
+#ifndef VALIDACAO_H
+#define VALIDACAO_H
 
+#include "tipos.h"  /* Deve declarar DataHora e os enums usados */
+
+int texto_valido(const char *s);
+
+int validar_datahora(DataHora d);
+
+int validar_prioridade(Prioridade p);
+int validar_tipo_ocorrencia(TipoOcorrencia t);
+int validar_estado_ocorrencia(EstadoOcorrencia e);
+
+int validar_estado_intervencao(EstadoIntervencao e);
+
+int validar_especialidade(Especialidade e);
+int validar_estado_bombeiro(EstadoBombeiro e);
+
+int validar_estado_equipamento(EstadoEquip e);
+
+#endif /* VALIDACAO_H */
