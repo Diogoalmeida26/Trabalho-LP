56da1a528c81502c0a7de26cc992c3316b2cd01a
P1nt056 <ropinto06@gmail.com>
2025-12-14 14:02:39 +0000

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,1 +1,13 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "ocorrencia.h"
+#include "intervencao.h"
+#include "bombeiro.h"
+#include "equipamento.h"
+#include "relatorios.h"
+
+void correr_menu(OcorrenciaRepo *or, IntervencaoRepo *ir, BombeiroRepo *br, EquipamentoRepo *er);
+
+#endif /* MENU_H */
 
